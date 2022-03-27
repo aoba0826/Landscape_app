c2f1f44bfdadf5be2480a78b761e17ba264f54a0
aoba0826 <werwerowner@gmail.com>
2022-03-27 07:37:53 +0000

    fix like button destroy 修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,8 +11,7 @@ class LikesController < ApplicationController
 
   def destroy
     @post_image = PostImage.find(params[:post_image_id])
-    like = Like.find_by(post_image_id: @post_image.id)
-    like.user_id = current_user.id
+    like = Like.find_by(post_image_id: @post_image.id,user_id: current_user.id)
     like.destroy
   end
 end
