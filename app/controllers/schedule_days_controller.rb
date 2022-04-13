a57254512bc22f9e705a32fb1bad8686cf44027c
aoba0826 <werwerowner@gmail.com>
2022-04-13 12:55:37 +0000

    Add スケジュールDAY 保存　追加

--- app/controllers/schedule_days_controller.rb
+++ app/controllers/schedule_days_controller.rb
@@ -1,19 +1,18 @@
 class ScheduleDaysController < ApplicationController
   def create
-    @schedule_day = ScheduleDay.new(schedule_day_params)
-    @schedule_day.user_id = current_user.id
-    @schedule_day.save
+    @schedule_day = current_user.schedule_days.find_or_create_by(schedule_day_params)
     render :index
   end
 
   def index
+    @schedule_day
   end
 
   def destroy
   end
 
   private
-  
+
   def schedule_day_params
     params.require(:schedule_day).permit(:task_id,:schedule_day)
   end
