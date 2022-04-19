04b6b239d6587ecc19e4fe66fcd905763b64a4c0
aoba0826 <werwerowner@gmail.com>
2022-04-19 06:49:40 +0000

    fix day_task保存時　に曜日をschedule_daｙの日付に変更　method 作成

--- app/controllers/day_tasks_controller.rb
+++ app/controllers/day_tasks_controller.rb
@@ -2,11 +2,13 @@ class DayTasksController < ApplicationController
 
   def create
     @day_task = current_user.day_tasks.new(day_task_params)
+    @day_task.set_the_day_implement
     @day_task.save
     redirect_to schedule_day_path(@day_task.schedule_day_id)
   end
 
   private
+
   def day_task_params
     params.require(:day_task).permit(:schedule_day_id,:task_contents,:start_time,:end_time)
   end
--- app/models/day_task.rb
+++ app/models/day_task.rb
@@ -1,5 +1,15 @@
 class DayTask < ApplicationRecord
   belongs_to :user
   belongs_to :schedule_day
+
+  def set_the_day_implement
+    year = self.schedule_day.schedule_day.year
+    month = self.schedule_day.schedule_day.month
+    day = self.schedule_day.schedule_day.day
+
+    self.start_time = self.start_time.change(year: year, month: month, day: day)
+    self.end_time = self.end_time.change(year: year, month: month, day: day)
+  end
+
 end
 
