5e92143d16257ac8bddaad51602ce4b07bae5765
aoba0826 <werwerowner@gmail.com>
2022-04-20 01:36:40 +0000

    fix daytask 変数変更

--- app/controllers/schedule_days_controller.rb
+++ app/controllers/schedule_days_controller.rb
@@ -2,17 +2,14 @@ class ScheduleDaysController < ApplicationController
   def create
     @schedule_day = current_user.schedule_days.find_or_create_by(schedule_day_params)
     @day_task = DayTask.new
-    @day_tasks = current_user.day_tasks.all
+    @day_tasks = current_user.day_tasks.where(schedule_day_id: @schedule_day.id).order(start_time: :ASC)
     render :show
   end
 
   def show
     @schedule_day = ScheduleDay.find(params[:id])
     @day_task = DayTask.new
-    @day_tasks = current_user.day_tasks.all.order(start_time: :ASC)
-  end
-
-  def destroy
+    @day_tasks = current_user.day_tasks.where(schedule_day_id: @schedule_day.id).order(start_time: :ASC)
   end
 
   private
